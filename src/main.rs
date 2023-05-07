88557acd835bcb69bb42347a557910f165fde248
kratos2377 <shobityadav23@gmail.com>
2023-05-07 19:25:39 +0530

    Mod files added

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
 use std::{env, time::Duration, path::PathBuf};
-fn main() {
+
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::post,
+};
+
+use mongodb::{options::{ClientOptions, Compressor}, Client};
+
+use tower_http::cors::{Any, CorsLayer};
+
+use axum_server::tls_rustls::RustlsConfig;
+
+mod controllers;
+use controllers::{auth_controller, fs_controller};
+
+#[tokio::main]
+async fn main() -> Result<() , Box<dyn std::error::Error> {
     println!("Hello, world!");
 }
