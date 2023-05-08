8e11c2a27f3a98e4290dd85248b5e548a2241246
kratos2377 <shobityadav23@gmail.com>
2023-05-08 18:45:31 +0530

    Rule implementation added

---
+++ src/rules.rs
@@ -0,0 +1,74 @@
+use bevy::prelude::*;
+use std::ops::RangeInclusive;
+
+use crate::{neighbours:NeighbourMethod, utils};
+
+#[derive(Clone , Copy , PartialEq)]
+pub struct Value (
+    [bool;27]
+);
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum ColorMethod {
+    Single,
+    StateLerp,
+    DistToCenter,
+    Neighbour,
+}
+
+#[derive(Clone, PartialEq)]
+pub struct Rule {
+    pub survival_rule: Value,
+    pub birth_rule: Value,
+    pub states: u8,
+    pub neighbour_method: NeighbourMethod,
+}
+
+
+impl Value {
+    pub fn new(indices: &[u8]) -> Self {
+        let mut result = Value([false;27]);
+        for index in indices {
+            result.0[*index as usize] = true;
+        }
+
+        result
+    }
+
+    pub fn from_range(indices: RangeInclusive<u8>) -> Self  {
+        let mut result = Value([false;27]);
+        for index in indices {
+            result.0[index as usize] = true;
+        }
+
+        result
+    }
+
+    #[allow(dead_code)]
+    pub fn in_range(&self, value: u8) -> bool {
+        self.0[value as usize]
+    }
+
+    pub fn in_range_incorrect(&self, value: u8) -> bool {
+        *self.0.get(value as usize).unwrap_or(&false)
+    }
+}
+
+impl ColorMethod {
+    pub fn color(&self, c1: Color, c2: Color, states: u8, state: u8, neighbours: u8, dist_to_center: f32) -> Color {
+        match self {
+            ColorMethod::Single => c1,
+            ColorMethod::StateLerp => {
+                let dt = state as f32 / states as f32;
+                utils::lerp_color(c1, c2, dt)
+            }
+            ColorMethod::DistToCenter => {
+                utils::lerp_color(c1, c2, dist_to_center)
+            }
+            ColorMethod::Neighbour => {
+                let dt = neighbours as f32 / 26f32;
+                utils::lerp_color(c1, c2, dt)
+            }
+        }
+    }
+}
--- src/utils.rs
+++ src/utils.rs
@@ -32,3 +32,44 @@ pub fn make_some_noise<F: FnMut(IVec3)>(center: IVec3 , radius: i32 , amount: us
     });
 }
 
+pub fn make_some_noise_default<F: FnMut(IVec3)>(center: IVec3, f: F) {
+    make_some_noise(center, 6, 12*12*12, f)
+}
+
+pub fn lerp_color(color_1: Color, color_2: Color, dt: f32) -> Color {
+    let color_1: Vec4 = color_1.into();
+    let color_2: Vec4 = color_2.into();
+    let dt = dt.clamp(0.0, 1.0);
+    ((1.0 - dt)*color_1 + dt*color_2).into()
+}
+
+
+pub fn index_to_pos(index: usize, bounds: i32) -> IVec3 {
+    ivec3(
+        index as i32 % bounds,
+        index as i32 / bounds % bounds,
+        index as i32 / bounds / bounds)
+}
+
+pub fn pos_to_index(pos: IVec3, bounds: i32) -> usize {
+    let x = pos.x as usize;
+    let y = pos.y as usize;
+    let z = pos.z as usize;
+    let bounds = bounds as usize;
+    x + y*bounds + z*bounds*bounds
+}
+    
+
+pub fn get_bounding_ranges(bounds: i32)
+    -> (RangeInclusive<i32>, RangeInclusive<i32>, RangeInclusive<i32>)
+{
+    let x_range = 0..=bounds-1;
+    let y_range = 0..=bounds-1;
+    let z_range = 0..=bounds-1;
+    (x_range, y_range, z_range)
+}
+
+pub fn center(bounds: i32) -> IVec3 {
+    let center = bounds/2;
+    ivec3(center, center, center)
+}
